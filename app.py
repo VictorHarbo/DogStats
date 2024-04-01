fe52ddd0f0b5a12ef3c0254e94d3a17964a13d70
Victor Harbo Johnston <vholesen@hotmail.com>
2024-04-01 14:50:27 +0200

    Add ChartJS chart to index page

--- app.py
+++ app.py
@@ -7,8 +7,26 @@ from dogbase import overview, all_steps, steps_before_dog, steps_since_dog
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return render_template('index.html', message='Dog Stats')
+def homepage():
+    # Define Plot Data 
+    
+    steps_with_dog = steps_since_dog()
+    
+    
+    labels = []
+    data = []
+    
+    for entry in steps_with_dog:
+        labels.append(entry)
+        data.append(steps_with_dog[entry])
+ 
+    # Return the components to the HTML template 
+    return render_template(
+        template_name_or_list='index.html',
+        data=data,
+        labels=labels,
+    )
+
 
 @app.route('/overview')
 def database():
@@ -27,3 +45,8 @@ def steps_without_dog():
 @app.route('/after')
 def steps_after_dog():
     return steps_since_dog()
+
+# Main Driver Function 
+if __name__ == '__main__':
+    # Run the application on the local development server ##
+    app.run(debug=True)
